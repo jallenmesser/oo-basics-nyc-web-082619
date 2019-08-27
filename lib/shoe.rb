0a551dc827c467ef2be718d5cc5413e268c9760d
Jallen Messersmith <jallenmessersmith@gmail.com>
2019-08-27 14:55:41 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,9 +1,13 @@
 # Make your shoe class here!
 class Shoe 
-  attr_accessor 
+  attr_accessor :color :size :material :condition
   attr_reader :brand
   
   def initialize(brand)
     @brand = brand
+  end
+  
+  def cobble
+    puts 
   end
 end
